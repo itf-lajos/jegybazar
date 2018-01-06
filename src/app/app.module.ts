81afbdfc64be7d1462ea312f7fa1502c8361521e
Lajos <itf.lajos@gmail.com>
2018-01-06 12:40:59 +0100

    routing & navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
+///<reference path="app-routing.module.ts"/>
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {CollapseModule} from 'ngx-bootstrap';
+import {AppRoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import {CollapseModule} from 'ngx-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { EventcardComponent } from './eventcard/eventcard.component';
 import { FooterComponent } from './footer/footer.component';
-
 
 @NgModule({
   declarations: [
@@ -16,11 +16,13 @@ import { FooterComponent } from './footer/footer.component';
     NavbarComponent,
     JumbotronComponent,
     EventcardComponent,
-    FooterComponent
+    FooterComponent,
+    ...AppRoutingModule.routableComponents
   ],
   imports: [
     BrowserModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
