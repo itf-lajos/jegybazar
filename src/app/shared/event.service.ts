a8af378246a49cfacfe809053faeb11a3efdeef9
Lajos <itf.lajos@gmail.com>
2018-01-08 21:21:19 +0100

    Event-ek kezelése

---
+++ src/app/shared/event.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import {EventModel} from './event-model';
+
+@Injectable()
+export class EventService {
+  private _events: EventModel[];
+
+  constructor() {
+    this._events = [
+      {
+        'id': 1,
+        'name': 'Sziget Fesztivál',
+        'date': '2017-08-03',
+        'pictureURL': 'assets/sziget.png',
+        'description': 'Nyári fesztivál a Hajógyári szigeten.'
+      },
+      {
+        'id': 2,
+        'name': 'Diótörő Balett',
+        'date': '2017-11-23',
+        'pictureURL': 'assets/diotoro.jpg',
+        'description': 'Fantasztikus balett előadás neves művészekkel.'
+      },
+      {
+        'id': 3,
+        'name': 'Macskák Musical',
+        'date': '2018-02-11',
+        'pictureURL': 'assets/macskak.jpg',
+        'description': 'Az ismert musical a Madách Színház művészeinek előadásában.'
+      },
+      {
+        'id': 4,
+        'name': 'Sziget Fesztivál',
+        'date': '2017-08-03',
+        'pictureURL': 'assets/sziget.png',
+        'description': 'Nyári fesztivál a Hajógyári szigeten.'
+      },
+      {
+        'id': 5,
+        'name': 'Diótörő Balett',
+        'date': '2017-11-23',
+        'pictureURL': 'assets/diotoro.jpg',
+        'description': 'Fantasztikus balett előadás neves művészekkel.'
+      },
+      {
+        'id': 6,
+        'name': 'Macskák Musical',
+        'date': '2018-02-11',
+        'pictureURL': 'assets/macskak.jpg',
+        'description': 'Az ismert musical a Madách Színház művészeinek előadásában.'
+      },
+      {
+        'id': 7,
+        'name': 'Sziget Fesztivál',
+        'date': '2017-08-03',
+        'pictureURL': 'assets/sziget.png',
+        'description': 'Nyári fesztivál a Hajógyári szigeten.'
+      },
+      {
+        'id': 8,
+        'name': 'Diótörő Balett',
+        'date': '2017-11-23',
+        'pictureURL': 'assets/diotoro.jpg',
+        'description': 'Fantasztikus balett előadás neves művészekkel.'
+      },
+      {
+        'id': 9,
+        'name': 'Macskák Musical',
+        'date': '2018-02-11',
+        'pictureURL': 'assets/macskak.jpg',
+        'description': 'Az ismert musical a Madách Színház művészeinek előadásában.'
+      }
+    ];
+
+  }
+
+  getAllEvents(): EventModel[] {
+    return this._events;
+  }
+
+}
