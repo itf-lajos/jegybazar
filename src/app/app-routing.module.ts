cc7eaaf86dbe779a77c7d5de915d493b4eff1333
Lajos <itf.lajos@gmail.com>
2018-01-08 09:16:49 +0100

    Refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
 import {EventComponent} from './event/event.component';
 import {TicketComponent} from './ticket/ticket.component';
 import {AboutComponent} from './about/about.component';
-import {LoginComponent} from './login/login.component';
-import {RegistrationComponent} from './registration/registration.component';
-import {EventListComponent} from './event-list/event-list.component';
-import {EventDetailComponent} from './event-detail/event-detail.component';
+import {LoginComponent} from './user/login/login.component';
+import {RegistrationComponent} from './user/registration/registration.component';
+import {EventListComponent} from './event/event-list/event-list.component';
+import {EventDetailComponent} from './event/event-detail/event-detail.component';
+import {ProfileComponent} from './user/profile/profile.component';
+import {ProfileEditComponent} from './user/profile-edit/profile-edit.component';
+import {TicketListComponent} from './ticket/ticket-list/ticket-list.component';
+import {TicketDetailComponent} from './ticket/ticket-detail/ticket-detail.component';
+import {BidComponent} from './ticket/bid/bid.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -16,13 +21,29 @@ const routes: Routes = [
     children: [
       {path: 'list', component: EventListComponent},
       {path: 'new', component: EventDetailComponent},
-      {path: ':id/edit', component: EventDetailComponent}
+      {path: ':id', component: EventDetailComponent}
     ]
   },
-  {path: 'ticket', component: TicketComponent},
+  {path: 'ticket', component: TicketComponent,
+    children: [
+      {path: 'list', component: TicketListComponent},
+      {path: 'new', component: TicketDetailComponent},
+      {path: ':id', component: TicketDetailComponent}
+    ]
+  },
+  {path: 'bid', component: BidComponent},
   {path: 'about', component: AboutComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registration', component: RegistrationComponent},
+  {
+    path: 'user',
+    children: [
+      {path: '', component: ProfileComponent},
+      {path: 'edit', component: ProfileEditComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'registration', component: RegistrationComponent},
+    ]
+  },
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent},
 ];
@@ -38,9 +59,14 @@ export class AppRoutingModule {
     EventListComponent,
     EventDetailComponent,
     TicketComponent,
+    TicketListComponent,
+    TicketDetailComponent,
+    BidComponent,
     AboutComponent,
     LoginComponent,
     RegistrationComponent,
+    ProfileComponent,
+    ProfileEditComponent,
     PageNotFoundComponent
   ];
 
