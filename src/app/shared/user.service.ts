68824cbddd827ae6fde2be75a2e08992f35575e5
Lajos <itf.lajos@gmail.com>
2018-01-09 08:57:39 +0100

    User kezelés

---
+++ src/app/shared/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+import {UserModel} from './user-model';
+
+@Injectable()
+export class UserService {
+  isLoggedin = false;
+
+  private _user: UserModel;
+
+  constructor(private _router: Router) { }
+
+  login(email: string, password: string) {
+    if (email === 'angular' && password === 'angular') {
+      this._user = new UserModel(UserModel.exampleUser);
+      this.isLoggedin = true;
+      this._router.navigate(['/user']);
+    }
+    return false;
+  }
+
+  register(param?: UserModel) {
+    if (param) {
+      this._user = new UserModel(param);
+    } else {
+      this._user = new UserModel(UserModel.exampleUser);
+    }
+    this.isLoggedin = true;
+    this._router.navigate(['/user']);
+  }
+
+  logout() {
+    this._user = new UserModel();
+    // delete(this._user);
+    this.isLoggedin = false;
+    this._router.navigate(['/home']);
+
+  }
+
+}
